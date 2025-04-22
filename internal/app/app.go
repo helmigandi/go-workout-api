0575dea1e61ab84328ce9d8d8cba6e57794905ab
helmigandi <sugandihelmi@gmail.com>
2025-04-22 23:50:54 +0700

    Add database layer

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,8 +1,11 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/helmigandi/go-workout-api/internal/api"
+	"github.com/helmigandi/go-workout-api/internal/store"
+	"github.com/helmigandi/go-workout-api/migrations"
 	"log"
 	"net/http"
 	"os"
@@ -11,10 +14,21 @@ import (
 type Application struct {
 	Logger         *log.Logger
 	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 // NewApplication creates a formatted print line across the application.
 func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
+	}
+	
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	// our stores will go here
@@ -25,6 +39,7 @@ func NewApplication() (*Application, error) {
 	return &Application{
 		Logger:         logger,
 		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
 	}, nil
 }
 
