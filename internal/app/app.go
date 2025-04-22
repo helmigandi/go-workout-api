6fa5bc366fb08edbdc6cd4f844cd625a42ce0840
helmigandi <sugandihelmi@gmail.com>
2025-04-22 19:49:43 +0700

    Add Chi API routes handlers

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,17 +1,33 @@
 package app
 
 import (
+	"fmt"
+	"github.com/helmigandi/go-workout-api/internal/api"
 	"log"
+	"net/http"
 	"os"
 )
 
 type Application struct {
-	Logger *log.Logger
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
 }
 
 // NewApplication creates a formatted print line across the application.
 func NewApplication() (*Application, error) {
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// our stores will go here
+
+	// our handlers will go here
+	workoutHandler := api.NewWorkoutHandler()
+
 	return &Application{
-		Logger: log.New(os.Stdout, "", log.Ldate|log.Ltime),
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
 	}, nil
+}
+
+func (a *Application) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status available\n")
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/helmigandi/go-workout-api/internal/app"
+	"github.com/helmigandi/go-workout-api/internal/routes"
 	"net/http"
 	"time"
 )
@@ -12,30 +13,26 @@ func main() {
 	var port int
 	flag.IntVar(&port, "port", 8080, "port to listen on")
 	flag.Parse()
-	fmt.Printf("Listening on port %d\n", port)
 
 	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
 	}
 
-	http.HandleFunc("/health", HealthCheck)
+	r := routes.SetupRoutes(app)
 
 	server := &http.Server{
 		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      r,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
 	app.Logger.Printf("Starting server on port %d", port)
-	
+
 	err = server.ListenAndServe()
 	if err != nil {
 		app.Logger.Fatal(err)
 	}
-}
-
-func HealthCheck(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Status available\n")
 }
