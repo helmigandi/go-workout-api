6ef5d60e984beb36fb140312f4c251321aa747b5
helmigandi <sugandihelmi@gmail.com>
2025-04-25 08:27:51 +0700

    Add testing

--- go.mod
+++ go.mod
@@ -3,16 +3,22 @@ module github.com/helmigandi/go-workout-api
 go 1.24.1
 
 require (
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/jackc/pgx/v4 v4.18.3
+	github.com/pressly/goose/v3 v3.24.2
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/ClickHouse/ch-go v0.65.1 // indirect
 	github.com/ClickHouse/clickhouse-go/v2 v2.33.1 // indirect
 	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
 	github.com/coder/websocket v1.8.13 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/elastic/go-sysinfo v1.15.2 // indirect
 	github.com/elastic/go-windows v1.0.2 // indirect
-	github.com/go-chi/chi/v5 v5.2.1 // indirect
 	github.com/go-faster/city v1.0.1 // indirect
 	github.com/go-faster/errors v0.7.1 // indirect
 	github.com/go-sql-driver/mysql v1.9.1 // indirect
@@ -26,8 +32,7 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgtype v1.14.0 // indirect
-	github.com/jackc/pgx/v4 v4.18.3 // indirect
+	github.com/jackc/pgtype v1.14.4 // indirect
 	github.com/jackc/pgx/v5 v5.7.4 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
@@ -41,7 +46,7 @@ require (
 	github.com/paulmach/orb v0.11.1 // indirect
 	github.com/pierrec/lz4/v4 v4.1.22 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pressly/goose/v3 v3.24.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/procfs v0.16.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
@@ -56,12 +61,12 @@ require (
 	go.opentelemetry.io/otel v1.35.0 // indirect
 	go.opentelemetry.io/otel/trace v1.35.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
 	golang.org/x/net v0.38.0 // indirect
-	golang.org/x/sync v0.12.0 // indirect
-	golang.org/x/sys v0.31.0 // indirect
-	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
 	google.golang.org/grpc v1.71.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
--- internal/store/workout_store_test.go
+++ internal/store/workout_store_test.go
@@ -2,6 +2,8 @@ package store
 
 import (
 	"database/sql"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -24,4 +26,116 @@ func setupTestDB(t *testing.T) *sql.DB {
 	}
 
 	return db
+}
+
+func TestCreateWorkout(t *testing.T) {
+	db := setupTestDB(t)
+	defer db.Close()
+
+	store := NewPostgresWorkoutStore(db)
+	tests := []struct {
+		name    string
+		workout *Workout
+		wantErr bool
+	}{
+		{
+			name: "valid workout",
+			workout: &Workout{
+				Title:           "push day",
+				Description:     "upper body day",
+				DurationMinutes: 60,
+				CaloriesBurned:  200,
+				Entries: []WorkoutEntry{
+					{
+						ExerciseName: "Bench Press",
+						Sets:         3,
+						Reps:         createIntPtr(10),
+						Weight:       createFloatPtr(85.9),
+						Notes:        "warm up properly",
+						OrderIndex:   1,
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "workout with invalid entries",
+			workout: &Workout{
+				Title:           "full body",
+				Description:     "full body day",
+				DurationMinutes: 90,
+				CaloriesBurned:  500,
+				Entries: []WorkoutEntry{
+					{
+						ExerciseName: "Plank",
+						Sets:         3,
+						Reps:         createIntPtr(60),
+						Notes:        "keep form",
+						OrderIndex:   1,
+					},
+					{
+						ExerciseName:    "Squats",
+						Sets:            4,
+						Reps:            createIntPtr(12),
+						DurationSeconds: createIntPtr(60),
+						Weight:          createFloatPtr(101.1),
+						OrderIndex:      2,
+					},
+				},
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := store.CreateWorkout(tt.workout)
+			// without assertions
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CreateWorkout() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			// with assertions
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+			assert.Equal(t, tt.workout.Title, got.Title)
+			assert.Equal(t, tt.workout.Description, got.Description)
+			assert.Equal(t, tt.workout.DurationMinutes, got.DurationMinutes)
+			assert.Equal(t, tt.workout.CaloriesBurned, got.CaloriesBurned)
+			assert.Equal(t, len(tt.workout.Entries), len(got.Entries))
+
+			savedWorkout, err := store.GetWorkoutByID(int64(got.ID))
+			require.NoError(t, err)
+			assert.Equal(t, got.ID, savedWorkout.ID)
+			assert.Equal(t, got.Title, savedWorkout.Title)
+			assert.Equal(t, got.Description, savedWorkout.Description)
+			assert.Equal(t, got.DurationMinutes, savedWorkout.DurationMinutes)
+			assert.Equal(t, got.CaloriesBurned, savedWorkout.CaloriesBurned)
+			assert.Equal(t, len(got.Entries), len(savedWorkout.Entries))
+
+			for i, entry := range got.Entries {
+				assert.Equal(t, entry, savedWorkout.Entries[i])
+				assert.Equal(t, entry.OrderIndex, savedWorkout.Entries[i].OrderIndex)
+				assert.Equal(t, entry.ExerciseName, savedWorkout.Entries[i].ExerciseName)
+				assert.Equal(t, entry.Sets, savedWorkout.Entries[i].Sets)
+				assert.Equal(t, entry.Reps, savedWorkout.Entries[i].Reps)
+				assert.Equal(t, entry.Weight, savedWorkout.Entries[i].Weight)
+				assert.Equal(t, entry.Notes, savedWorkout.Entries[i].Notes)
+				assert.Equal(t, entry.DurationSeconds, savedWorkout.Entries[i].DurationSeconds)
+			}
+		})
+	}
+}
+
+func createIntPtr(i int) *int {
+	return &i
+}
+
+func createFloatPtr(f float64) *float64 {
+	return &f
 }
